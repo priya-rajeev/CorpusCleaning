555a700d0125152f0154b582f64bca344857eca9
priya-rajeev <prajeev3@gatech.edu>
2023-02-21 00:47:59 -0500

    logic for getting responses updated, post next

--- data_cleaning.js
+++ data_cleaning.js
@@ -1,3 +1,4 @@
+//chooses random elements in an array
 function chooseRandom(arr, num) {
     if (num >= arr.length) {
         return arr;
@@ -6,6 +7,7 @@ function chooseRandom(arr, num) {
     return shuffled.slice(0, num);
 };
 
+//fetches entries from the database
 async function getEntriesForReview() {
     //     fetch('http://localhost:3000/api/getAll')
     //     .then( res => res.json())
@@ -24,14 +26,36 @@ async function getEntriesForReview() {
     return entries;
 }
 
-function collectData() {
-    //get id/name
+//dynamically adjusts height of text areas
+function adjustHeight(inputField) {
+    inputField.style.height = "1px";
+    inputField.style.height = (25+inputField.scrollHeight)+"px";
+}
 
-    //get sentence breakdowns and pos
+//posts data to the database
+function postData(sentences) {
+    var text_areas = Array.from(document.getElementsByTagName("textarea"));
+
+    //get id/name
+    var id = text_areas.shift().value;
+    console.log('id number: ', id);
+
+    sentence_segmentations = [];
+    //get sentence segmentations
+    text_areas.forEach(ta => {
+        sentence_segmentations.push(ta.value);
+    })
+
+    for(var i=0; i < sentences.length; i++) {
+        sentences[i]['sentence_segmentation'] = sentence_segmentations[i];
+        sentences[i]['reviewer_id'] = id.toString();
+    }
 
     //post to db
+    console.log('reviewed: ', sentences);
 };
 
+//renders the page
 async function getCleaningTask(seg_container) {
 
     //identify rows in which segmentation has not been completed yet
@@ -39,30 +63,44 @@ async function getCleaningTask(seg_container) {
     
     //randomly select 10 choices
     to_review = chooseRandom(entries, 10);
+    console.log('to review: ', to_review);
 
     //display 10 sentences
     var output = [];
     output.push('<form class="quiz-container">');
 
+    //ask for id number
+    var id_prompt = '<div class="form-group" id="id_prompt">' +
+                        '<label for="reviewer_id">' +
+                            '<strong> Please enter your ID number below: </strong>'+
+                        '</label>'+
+                        '<textarea onkeyup="adjustHeight(this)" class="form-control" role="textbox" id="response_num_' +i+ '" rows="1"></textarea>'+
+                    '</div>';
+    output.push(id_prompt);
+
     //for each randomly selected task
     for(var i=0; i<to_review.length; i++){
         var curr_task = [];
         curr_task.push('<div class="form-group">' +
-                          '<label for="sentence_num_'+i+'">' +
-                            '<strong>Text: </strong>' + to_review[i]['sentence']+
+                            '<label for="sentence_num_'+i+'">' +
+                                '<strong>Text: </strong>' + to_review[i]['sentence']+
                             '</label>'+
-                            '<textarea class="form-control" id="response_num_' +i+ '" rows="4"></textarea>');
+                            '<textarea onkeyup="adjustHeight(this)" class="form-control" role="textbox" id="response_num_' +i+ '" rows="4"></textarea>'+
+                        '</div>');
        //add the current task to the output
         output.push(curr_task);
     }
 
     var submit_btn = '<div class="container" id="check_button">' +
-                        '<button id="check_btn" type="button" class="btn btn-primary">Check</button>' +
+                        '<button id="sub_btn" type="button" class="btn btn-primary">Submit</button>' +
                     '</div>';
     
     output.push(submit_btn);
     output.push('</form>');
-
-    console.log(output);
     seg_container.innerHTML = output.join('');
+
+    submit_btn = document.getElementById("sub_btn");
+    submit_btn.onclick = function(){
+        postData(to_review);
+    };
 }
