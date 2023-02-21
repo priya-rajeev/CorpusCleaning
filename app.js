91f09c4a9cc4660c8b2872f24abbdbc7530082b6
priya-rajeev <prajeev3@gatech.edu>
2023-02-20 22:48:19 -0500

    pulling data from database, randomly generate 10 entries

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
--- data_cleaning.js
+++ data_cleaning.js
@@ -1,27 +1,27 @@
 function chooseRandom(arr, num) {
+    if (num >= arr.length) {
+        return arr;
+    }
     var shuffled = arr.sort(() => 0.5 - Math.random());
-    // console.log(shuffled.slice(0, num));
     return shuffled.slice(0, num);
-}
+};
 
-function getCleaningTask(sample_container) {
-    //pull all rows from database
-    fetch('/getAll')
-    .then( res => res.json())
-    .then((data) => {
-    //we are actually using the returned data from the API here
-    // data.results contains an array of objects so we can use an array method here to iterate
-        // data.results.forEach( (imageObj) =>{
-        //   createImage(imageObj);
-        // });
-        console.log(data.length)
+async function getEntriesForReview() {
+    //     fetch('http://localhost:3000/api/getAll')
+    //     .then( res => res.json())
+    //     .then((data) => {
+    //         database = data;
+    //     });
+    var db = await fetch('http://localhost:3000/api/getAll');
+    db = await db.json();
+    var entries = [];
+
+    db.forEach(row => {
+        if (row['sentence_segmentation'] == ' ') {
+            entries.push(row);
+        }
     });
-
-    //identify rows in which the second id column is empty 
-
-    //randomly select 10 choices
-
-    //display 10 choices 
+    return entries;
 }
 
 function collectData() {
@@ -30,4 +30,39 @@ function collectData() {
     //get sentence breakdowns and pos
 
     //post to db
+};
+
+async function getCleaningTask(seg_container) {
+
+    //identify rows in which segmentation has not been completed yet
+    entries = await getEntriesForReview();
+    
+    //randomly select 10 choices
+    to_review = chooseRandom(entries, 10);
+
+    //display 10 sentences
+    var output = [];
+    output.push('<form class="quiz-container">');
+
+    //for each randomly selected task
+    for(var i=0; i<to_review.length; i++){
+        var curr_task = [];
+        curr_task.push('<div class="form-group">' +
+                          '<label for="sentence_num_'+i+'">' +
+                            '<strong>Text: </strong>' + to_review[i]['sentence']+
+                            '</label>'+
+                            '<textarea class="form-control" id="response_num_' +i+ '" rows="4"></textarea>');
+       //add the current task to the output
+        output.push(curr_task);
+    }
+
+    var submit_btn = '<div class="container" id="check_button">' +
+                        '<button id="check_btn" type="button" class="btn btn-primary">Check</button>' +
+                    '</div>';
+    
+    output.push(submit_btn);
+    output.push('</form>');
+
+    console.log(output);
+    seg_container.innerHTML = output.join('');
 }
